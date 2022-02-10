2af11a36097b31e8b2ddc1aec0e4f65d45be0982
astronomija <aleksandraradosavljevic@live.com>
2022-02-10 11:50:27 +0100

    dodate metode sa izracunavanje prosecnog rezultata metoda optimizacije i metode za iscrtavanje grafika za rezultate i za rezultate po iteracijama

--- testing.py
+++ testing.py
@@ -48,4 +48,77 @@ def compare(f1,f2,edges=10,nodes=10,iterations=100):
             results['equal'] += 1
     print(results)
 
-compare(gen.genetic_search, an.simulated_annealing_sort)
+#compare(gen.genetic_search, an.simulated_annealing_sort)
+
+def avg_results(functions, edges=10, nodes=10, iterations=10):
+    results = {}
+    sums = {}
+    for f in functions:
+        results[f.__name__] = []
+        sums[f.__name__] = 0
+    for i in range(0,iterations):
+        graph = createGraph(edges, nodes)
+        for f in functions:
+            res = f(graph)[1]
+            results[f.__name__].append(res)
+            sums[f.__name__] += res
+    plt.figure(figsize=(8, 6))
+    ax = plt.subplot(111)
+    averages = []
+    _averages = {}
+
+    for search_f in functions:
+        name = search_f.__name__
+        averages.append(sums[name] / iterations)
+        print(name, averages[-1])
+        _averages[name] = averages[-1]
+        plt.bar([name], averages[-1])
+
+    plt.grid(True, which='both', axis='y', zorder=0, alpha=1)
+    plt.grid(which='minor', alpha=.3)
+    major_ticks = np.arange(0, max(averages) + 1, 5)
+    minor_ticks = np.arange(0, max(averages) + 1, 1)
+    ax.set_yticks(major_ticks)
+    ax.set_yticks(minor_ticks, minor=True)
+    plt.legend(list(map(lambda f: f.__name__ + ' ' + str(_averages[f.__name__]), functions)), loc='upper center', bbox_to_anchor=(0.5, 0), ncol=2)
+    plt.tick_params(axis='x', bottom=False, labelbottom=False)
+    plt.title('Average for {} nodes and {} edges in {} iterations\n'.format(nodes, edges, iterations))
+    plt.show()
+
+avg_results([an.simulated_annealing, gen.genetic_search])
+
+def plot_results(function, edges= 10, nodes= 10, iterations = 100):
+    results = []
+    # graph = create_graph(edges, nodes)
+    for i in range(0,iterations):
+        print(i)
+        graph = createGraph(edges, nodes)
+        results.append(function(graph)[1])
+
+    plt.figure(figsize=(12, 6))
+    ax = plt.subplot(111)
+    ax.plot(range(1,iterations + 1), results)
+    plt.grid(True, which='both', axis='y', zorder=0, alpha=1)
+    major_ticks = np.arange(min(results)-1, max(results)+1, 1)
+    ax.set_yticks(major_ticks)
+    ax.set_label(function.__name__)
+    plt.title("{} : {} nodes / {} edges".format(function.__name__, nodes, edges))
+    plt.show()
+
+def plot_results_by_iter(function, edges= 10, nodes= 10, iterations = 100):
+    results = []
+    graph = createGraph(edges, nodes)
+    for i in range(0,iterations):
+        print(i)
+        results.append(function(graph, iterations = i)[1])
+
+    plt.figure(figsize=(12, 6))
+    ax = plt.subplot(111)
+    ax.plot(range(1,iterations + 1), results)
+    plt.grid(True, which='both', axis='y', zorder=0, alpha=1)
+    major_ticks = np.arange(min(results)-2, max(results)+2, 1)
+    ax.set_yticks(major_ticks)
+    ax.set_label(function.__name__)
+    plt.title("{} - Results by iterations \n{} nodes / {} edges".format(function.__name__, nodes, edges))
+
+    plt.show()
